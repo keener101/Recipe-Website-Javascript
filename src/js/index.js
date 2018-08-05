8485c082fad8518159a4bbeb28904af68ac05a4a
Adam Keener <keener101@gmail.com>
2018-08-05 15:15:08 -0600

    UI list and loader animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './models/Search';
+import { elements, displayLoader, clearLoader } from './view/base';
+import * as searchView from './view/searchView';
 
 /*Global State of app
     -Search Object
@@ -12,23 +14,27 @@ const state = {};
 
 const controlSearch = async () => {
     //1 Get Query from view
-    const query = 'creamed spinach'; //todo
+    const query = searchView.getInput();
 
     if(query){
         //2 new search obj, add to state
         state.search = new Search(query);
         
         //3 prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        displayLoader(elements.searchRes);
 
         //4 search for recipes 
         await state.search.getResults();
 
         //5 display results on UI
-        console.log(state.search.result);
+        clearLoader();
+        searchView.displayResults(state.search.result);
     }
 } 
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 })
